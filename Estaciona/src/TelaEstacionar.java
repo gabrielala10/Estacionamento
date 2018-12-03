702d1463ceca80b684aa7a4c861265d6bb1d75fa
gabrielala10 <gabriela_m18@outlook.com>
2018-12-03 15:06:38 -0200

    Add files via upload

--- Estaciona/src/TelaEstacionar.java
+++ Estaciona/src/TelaEstacionar.java
@@ -1,15 +1,15 @@
 
-import java.awt.BorderLayout;
-
-import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class TelaEstacionar extends JFrame{
 
+	JFrame janela;
+	
 	public void estaciona()
 	{
-		JFrame janela = new JFrame("Inserir Veículo");
+		janela = new JFrame("Inserir Veículo");
 	    janela.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 	    
 	    
@@ -17,8 +17,14 @@ public class TelaEstacionar extends JFrame{
 	    janela.setLocationRelativeTo(null);
 	    janela.setVisible(true);
 	    
-	    
-	    
+	}
+	
+	private void text()
+	{
+		JPanel titulo = new JPanel();
+		JLabel inserir = new JLabel("Inserir Veículo");
+		titulo.add(inserir);
+		janela.add(titulo);
 	}
 	
 	public void retirar()
--- Estaciona/src/TelaPrincipal.java
+++ Estaciona/src/TelaPrincipal.java
@@ -13,7 +13,7 @@ import javax.swing.SwingConstants;
 public class TelaPrincipal {
 	
 	JFrame janela;
-	JPanel panel;
+	JPanel panel = new JPanel(null);
 	
 	  public void plantaBaixa(Veiculo veiculos[])
 	  {
@@ -35,70 +35,83 @@ public class TelaPrincipal {
 	  
 	  private void quadrado(Veiculo veiculos[])
 	  {
-		  int x = 84, y = 50, i, j;
+		  int x = 90, y = 50, i, j, k;
 		  JPanel quad;
+		  JLabel text;
 		  for(j=0;j<10;j++)
 		  {
 			  for(i=0;i<20;i++)
 			  {
 				  
-				  Color corVago = new Color(4, 139, 34);
-				  Color corOcupado = new Color(255, 0, 0);
+				  Color corVago = new Color(46,139,87);
+				  Color corOcupado = new Color(205,92,92);
 				  quad = new JPanel();
 				  if(i<10)
 				  {
+					  text = new JLabel(""+ j+i);
 					  if(veiculos[j*10+i].getConcluido()==1)
 						  quad.setBackground( corVago );
 					  else
 						  quad.setBackground( corOcupado );
 					  quad.setLocation(x, y);
+					  quad.add(text);
 				  }
 				  else
 				  {
+					  k = i-10;
+					  text = new JLabel(""+ 1+j+k);
 					  if(veiculos[j*10+i+90].getConcluido()==1)
 						  quad.setBackground( corVago );
 					  else
 						  quad.setBackground( corOcupado );
+					  quad.add(text);
 				  }
 				  quad.setLocation(x, y);
 				  quad.setSize(40, 50);
 				  if(i!=9)
 					  x+=60;
 				  else
-					  x+=80;
+					  x+=70;
 				  janela.add(quad);
 				  
 			  }
-			  x = 84;
+			  x = 90;
 			  y += 60;
 		  }
 	  }
 	  
 	  private void textos()
 	  {
-		  Color cor = new Color(152, 251, 152);
+		  Color cor = new Color(176,196,222);
 
-		  JPanel text1, text2, text3, text4, text5, text6;
-		  JLabel terreo, piso1, motos, carros, caminhonetes;
+		  JPanel text1;
+		  JPanel text2;
+		  JPanel text3 = new JPanel(null);
+		  JPanel text4 = new JPanel(null);
+		  JPanel text5 = new JPanel(null);
+		  JPanel text6 = new JPanel(null);
+		  JLabel terreo, piso1, motos, carros, carros2, caminhonetes;
 		  terreo = new JLabel("Térreo");
 		  piso1 = new JLabel("Piso 1");
 		  motos = new JLabel("Motos");
 		  caminhonetes = new JLabel("Caminhonetes");
 		  carros = new JLabel("Carros");
+		  carros2 = new JLabel("Carros");
+		  
+		  motos.setBounds(25, 45, 40, 20);
+		  caminhonetes.setBounds(0, 45, 90, 20);
+		  carros.setBounds(23, 165, 40, 20);
+		  carros2.setBounds(23, 285, 40, 20);
 		  
 		  text1 = new JPanel();
 		  text2 = new JPanel();
-		  text3 = new JPanel();
-		  text4 = new JPanel();
-		  text5 = new JPanel();
-		  text6 = new JPanel();
 		  
-		  text1.setLocation(80,10);
+		  text1.setLocation(90,10);
 		  text2.setLocation(700,10);
 		  text3.setLocation(1,50);
 		  text4.setLocation(1,170);
-		  text5.setLocation(1,120);
-		  text6.setLocation(740,10);
+		  text5.setLocation(1,290);
+		  text6.setLocation(1284,50);
 		  
 		  text1.setBackground( cor );
 		  text2.setBackground( cor );
@@ -109,10 +122,10 @@ public class TelaPrincipal {
 		  
 		  text1.setSize(580, 30);
 		  text2.setSize(580, 30);
-		  text3.setSize(83, 110);
-		  text4.setSize(83, 110);
-		  text5.setSize(79, 50);
-		  text6.setSize(50, 700);
+		  text3.setSize(85, 110);
+		  text4.setSize(85, 110);
+		  text5.setSize(85, 350);
+		  text6.setSize(85, 590);
 		  
 		 
 		  text1.add(terreo);
@@ -120,7 +133,7 @@ public class TelaPrincipal {
 		  text3.add(motos);
 		  text4.add(caminhonetes);
 		  text5.add(carros);
-		  text6.add(carros);
+		  text6.add(carros2);
 		  motos.setVerticalAlignment(SwingConstants.CENTER);
 		  
 		  
@@ -128,8 +141,8 @@ public class TelaPrincipal {
 		  janela.add(text2);
 		  janela.add(text3);
 		  janela.add(text4);
-		  //janela.add(text5);
-		//  janela.add(text6);
+		  janela.add(text5);
+		  janela.add(text6);
 	  }
 	  
 	  private void criaMenu()
